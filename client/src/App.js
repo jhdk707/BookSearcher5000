3eab271969601f8e795ab823bf4edd207e897bf4
Doomtower <jessehudak707@outlook.com>
2023-05-26 19:40:07 -0700

    fixed package.json and server files/ app for react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -37,6 +37,28 @@ const client = new ApolloClient({
 
 function App() {
   return (
+    // <ApolloProvider client={client}>
+    //   <Router>
+    //     <>
+    //       <Navbar />
+    //       <Routes>
+    //         <Route
+    //           path='/'
+    //           element={<SearchBooks />}
+    //         />
+    //         <Route
+    //           path='/saved'
+    //           element={<SavedBooks />}
+    //         />
+    //         <Route
+    //           path='*'
+    //           element={<h1 className='display-2'>Wrong page!</h1>}
+    //         />
+    //       </Routes>
+    //     </>
+    //   </Router>
+    // </ApolloProvider>
+
     <ApolloProvider client={client}>
       <Router>
         <>
